5be7a9fac0b2035a9ed814f2da10ba904cb74c58
chetasr <me@chetasr.co>
2020-05-25 22:44:17 +0530

    mg347: added

--- manhuaplus.py
+++ manhuaplus.py
@@ -85,3 +85,83 @@ class ManhuaPlus(MangaSource):
             return {
                 'status': 'error'
         }
+
+
+class Manga347(MangaSource):
+    source_name = 'Manga347'
+    source_id = 'mg347'
+
+    base_url = 'https://manga347.com/'
+    mg_url = 'https://manga347.com/manga/{}/'
+    chp_url = 'https://manga347.com/manga/{}/{}/'
+    adm_ajax = 'https://manga347.com/wp-admin/admin-ajax.php'
+
+    requests = cfscrape.create_scraper()
+    
+    def updates(self, count=5):
+        mgd = []
+        soup = BeautifulSoup(self.requests.get(self.base_url).text, features='html.parser').find('div', {'class': 'page-content-listing'})
+        for x in soup.find_all('div', {'class': 'page-item-detail'}, limit=count):
+            img = 'https'+x.find('img')['data-src'].split('https')[-1].replace('175x238', '')
+            title = x.find('a')
+            chid = x.find('div', {'data-post-id': True})['data-post-id']
+            iden = title['href'].split('/')[-2]
+            time = BeautifulSoup(self.requests.post(self.adm_ajax, data={'action': 'manga_get_chapters', 'manga': chid}).text, features='html.parser').find('li', {'class': 'wp-manga-chapter'}).find('span')
+            title = title['title']
+            try:
+                time = time.find('i').text
+            except:
+                time = time.find('a')['title']
+            mgd.append({
+                'name': title,
+                'id': iden,
+                'cover': img,
+                'time': time
+            })
+        return mgd
+
+    def top(self, count=5):
+        # Does not have top
+        return updates(count)
+
+    def manga(self, iden):
+        try:
+            soup = BeautifulSoup(self.requests.get(self.mg_url.format(iden)).text, features='html.parser')
+            chid = soup.find('div', {'id': 'manga-chapters-holder'})['data-id']
+            chid = BeautifulSoup(self.requests.post(self.adm_ajax, data={'action': 'manga_get_chapters', 'manga': chid}).text, features='html.parser').find_all('li', {'class': 'wp-manga-chapter'})
+            chs = [{
+                'id': iden+';'+x.find('a')['href'].split('/')[-2],
+                'title': x.find('a').text.strip()
+            } for x in chid]
+            dat = {
+                'description': soup.find('div', {'class': 'summary__content'}).text,
+                'author': soup.find('div', {'class': 'author-content'}).text,
+                'chapters_number': len(chs),
+                'chapters': chs,
+                'genres': [x.text for x in soup.find('div', {'class': 'genres-content'}).find_all('a')]
+            }
+            return {
+                'status': 'ok', 
+                'result': dat
+            }
+        except Exception as e:
+            print(type(e), e, iden)
+            return {
+                'status': 'error'
+        }
+
+    def chapter(self, iden):
+        try:
+            mg, chp = iden.split(';')
+            url = self.chp_url.format(mg, chp)
+            pgs = BeautifulSoup(self.requests.get(url).text, features='html.parser').find('ul', {'class': 'blocks-gallery-grid'})
+            pgs = [x.find('img')['src'] for x in pgs.find_all('li')]
+            return {
+                'status': 'ok', 
+                'result': pgs
+            }
+        except Exception as e:
+            print(type(e), e, iden)
+            return {
+                'status': 'error'
+        }
